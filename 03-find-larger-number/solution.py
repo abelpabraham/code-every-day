db31bed89b39cdfc7129ac0798140a2e757d6e0d
abelpabraham <abelpabraham123@gmail.com>
2026-09-25 14:18:28 +0530

    Create solution.py

---
+++ 03-find-larger-number/solution.py
@@ -0,0 +1,9 @@
+A = int(input("Enter the first number: "))
+B = int(input("Enter the second number: "))
+
+if A > B:
+    print(f"{A} is larger")
+elif A < B:
+    print(f"{B} is larger")
+else:
+    print(f"{A} = {B}")
